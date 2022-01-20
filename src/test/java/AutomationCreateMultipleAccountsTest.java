448cb733afcae97a228c383f59c9ee046486a94a
Andy Kilgore <kilgoreandy@icloud.com>
2022-01-20 15:47:23 -0600

    got the multiple account to work with the csv however need to work on the selectbyvisible

--- src/test/java/AutomationCreateMultipleAccountsTest.java
+++ src/test/java/AutomationCreateMultipleAccountsTest.java
@@ -1,94 +1,109 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class AutomationCreateMultipleAccountsTest {
-    int count;
-    CSVReader excelopen = new CSVReader(new FileReader(fileName : "resources/AccountInfo.csv"));
-    String cell[];
+    String CSV_PATH = "resources/AccountInfo.csv";
+    WebDriver driver;
+    private CSVReader csvReader;
+    String[] csvCell;
+
+    @Before
+    public void setup() throws Exception {
+
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.com/index.php");
+    }
+
 
     @Test
-    public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        while ((cell = reader.readNext()) != null))
-        {
-            String gender = cell[0];
-            String fName = cell[1];
-            String lName = cell[2];
-            String email = cell[3];
-            String password = cell[4];
-            String birthday = cell[5];
-            String birthmonth = cell[6];
-            String birthyear = cell[7];
-            String newsletter = cell[8];
-            String offers = cell[9];
-            String addressFname = cell[1];
-            String addressLname = cell[2];
-            String company = cell[9];
-            String address1 = cell[10];
-            String city = cell[11];
-            String state = cell[12];
-            String zip = cell[13];
-            String country = cell[14];
-            String addinfo = cell[15];
-            String hphone = cell[16];
-            String mphone = cell[17];
-            String addressalias = cell[18];
+    public void firstTest() throws IOException, CsvValidationException {
 
-            driver.get("http://automationpractice.com/index.php");
-            driver.manage().window().setSize(new Dimension(1900, 1017));
-            driver.findElement(By.linkText("Sign in")).click();
-            driver.findElement(By.id("email_create")).click();
-            driver.findElement(By.id("email_create")).sendKeys(email);
-            driver.findElement(By.cssSelector("#SubmitCreate > span")).click();
-            driver.findElement(By.id("customer_firstname")).click();
+        csvReader = new CSVReader(new FileReader(CSV_PATH));
 
-            driver.findElement(By.id("customer_firstname")).sendKeys(fName);
-            driver.findElement(By.id("customer_lastname")).sendKeys(lName);
-            driver.findElement(By.id("passwd")).sendKeys(password);
-            Select day = new Select(driver.findElement(By.xpath("//*[@id='days']")));
-            day.selectByVisibleText(birthday);
-            Select month = new Select(driver.findElement(By.xpath("//*[@id='months']")));
-            month.selectByVisibleText(birthmonth);
-            Select year = new Select(driver.findElement(By.xpath("//*[@id='years']")));
-            year.selectByVisibleText(birthyear);
-            if (newsletter == "TRUE") {
-                driver.findElement(By.id("newsletter")).click();
+        while ((csvCell = csvReader.readNext()) != null) {
+
+                String gender = csvCell[0];
+                String fName = csvCell[1];
+                String lName = csvCell[2];
+                String email = csvCell[3];
+                String password = csvCell[4];
+                int birthday = Integer.valueOf(csvCell[5]);
+                String birthmonth = csvCell[6];
+                String birthyear = csvCell[7];
+                String newsletter = csvCell[8];
+                String offers = csvCell[9];
+                String addressFname = csvCell[1];
+                String addressLname = csvCell[2];
+                String company = csvCell[10];
+                String address1 = csvCell[11];
+                String city = csvCell[12];
+                String state = csvCell[13];
+                String zip = csvCell[14];
+                String country = csvCell[15];
+                String addinfo = csvCell[16];
+                String hphone = csvCell[17];
+                String mphone = csvCell[18];
+                String addressalias = csvCell[19];
+
+                driver.findElement(By.linkText("Sign in")).click();
+                driver.findElement(By.id("email_create")).click();
+                driver.findElement(By.id("email_create")).sendKeys(email);
+                driver.findElement(By.cssSelector("#SubmitCreate > span")).click();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.findElement(By.id("customer_firstname")).click();
+                driver.findElement(By.id("customer_firstname")).sendKeys(fName);
+                driver.findElement(By.id("customer_lastname")).sendKeys(lName);
+                driver.findElement(By.id("passwd")).sendKeys(password);
+
+                Select day = new Select(driver.findElement(By.xpath("//*[@id='days']")));
+                day.selectByVisibleText(birthday);
+                Select month = new Select(driver.findElement(By.xpath("//*[@id='months']")));
+                month.selectByVisibleText(birthmonth);
+                Select year = new Select(driver.findElement(By.xpath("//*[@id='years']")));
+                year.selectByVisibleText(birthyear);
+                if (newsletter == "TRUE") {
+                    driver.findElement(By.id("newsletter")).click();
+                }
+                if (offers == "TRUE") {
+                    driver.findElement(By.id("optin")).click();
+                }
+                driver.findElement(By.id("company")).click();
+                driver.findElement(By.id("company")).sendKeys(company);
+                driver.findElement(By.id("address1")).click();
+                driver.findElement(By.id("address1")).sendKeys(address1);
+                driver.findElement(By.id("city")).click();
+                driver.findElement(By.id("city")).sendKeys(city);
+                Select states = new Select(driver.findElement(By.xpath("//*[@id='id_state']")));
+                states.selectByVisibleText(state);
+                driver.findElement(By.id("postcode")).click();
+                driver.findElement(By.id("postcode")).sendKeys(zip);
+                driver.findElement(By.id("other")).click();
+                driver.findElement(By.id("other")).sendKeys(addinfo);
+                driver.findElement(By.id("phone")).click();
+                driver.findElement(By.id("phone")).sendKeys(hphone);
+                driver.findElement(By.id("phone_mobile")).click();
+                driver.findElement(By.id("phone_mobile")).sendKeys(mphone);
+                driver.findElement(By.cssSelector(".form-group:nth-child(13)")).click();
+                driver.findElement(By.id("alias")).click();
+                driver.findElement(By.id("alias")).sendKeys(addressalias);
+                driver.findElement(By.cssSelector("#submitAccount > span")).click();
+                driver.findElement(By.linkText("Sign out")).click();
+                driver.close();
+
             }
-            if (offers == "TRUE"){
-                driver.findElement(By.id("optin")).click();
-            }
-            driver.findElement(By.id("company")).click();
-            driver.findElement(By.id("company")).sendKeys(company);
-            driver.findElement(By.id("address1")).click();
-            driver.findElement(By.id("address1")).sendKeys(address1);
-            driver.findElement(By.id("city")).click();
-            driver.findElement(By.id("city")).sendKeys(city);
-            Select states = new Select(driver.findElement(By.xpath("//*[@id='id_state']")));
-            states.selectByVisibleText(state);
-            driver.findElement(By.id("postcode")).click();
-            driver.findElement(By.id("postcode")).sendKeys(zip);
-            driver.findElement(By.id("other")).click();
-            driver.findElement(By.id("other")).sendKeys(addinfo);
-            driver.findElement(By.id("phone")).click();
-            driver.findElement(By.id("phone")).sendKeys(hphone);
-            driver.findElement(By.id("phone_mobile")).click();
-            driver.findElement(By.id("phone_mobile")).sendKeys(mphone);
-            driver.findElement(By.cssSelector(".form-group:nth-child(13)")).click();
-            driver.findElement(By.id("alias")).click();
-            driver.findElement(By.id("alias")).sendKeys(addressalias);
-            driver.findElement(By.cssSelector("#submitAccount > span")).click();
-            driver.findElement(By.linkText("Sign out")).click();
-            driver.close();
         }
     }
-}
+
---
+++ src/test/java/DataDrivenTestingUsingCSVInSelenium.java
@@ -0,0 +1,66 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataDrivenTestingUsingCSVInSelenium {
+    //Provide test data CSV file path. As below path based on Mac machine. So, lets say you are using windows machine then write the below path accordingly.
+    String CSV_PATH = "resources/AccountInfo.csv";
+    WebDriver driver;
+    private CSVReader csvReader;
+    String[] csvCell;
+
+    @Before
+    public void setup() throws Exception {
+
+        //You can specify the hardcoded value of a chrome driver or driver based on your browser like below line
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
+        //OR
+
+        //Use below line to manage driver by WebdriverManager for chrome browser in our case (you can use any other driver of your choice)
+        //WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://amazon.com");
+    }
+
+    @Test
+    public void dataRead_CSV() throws IOException, CsvValidationException {
+        //Create an object of CSVReader
+        csvReader = new CSVReader(new FileReader(CSV_PATH));
+
+        driver.findElement(By.id("nav-link-accountList")).click();
+        driver.findElement(By.id("createAccountSubmit")).click();
+
+        //You can use while loop like below, It will be executed until the last line in CSV used.
+        while ((csvCell = csvReader.readNext()) != null) {
+            String CustomerName = csvCell[0];
+            String CustomerEmail = csvCell[1];
+            String CustomerPassword = csvCell[2];
+            String CustomerConfirmPassword = csvCell[3];
+            driver.findElement(By.id("ap_customer_name")).clear();
+            driver.findElement(By.id("ap_customer_name")).sendKeys(CustomerName);
+            driver.findElement(By.id("ap_email")).clear();
+            driver.findElement(By.id("ap_email")).sendKeys(CustomerEmail);
+            driver.findElement(By.id("ap_password")).clear();
+            driver.findElement(By.id("ap_password")).sendKeys(CustomerPassword);
+            driver.findElement(By.id("ap_password_check")).clear();
+            driver.findElement(By.id("ap_password_check")).sendKeys(CustomerConfirmPassword);
+            driver.findElement(By.id("continue")).click();
+        }
+    }
+
+    @After
+    public void exit() {
+        driver.close();
+        driver.quit();
+    }
+}
