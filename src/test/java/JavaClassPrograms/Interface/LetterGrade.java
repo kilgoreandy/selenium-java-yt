8749eed385ca4b1f962fe7f8d64ca1dbadb108d1
akilg <kilgoreandy@icloud.com>
2022-03-09 16:53:16 -0600

    revised

--- src/test/java/JavaClassPrograms/Interface/LetterGrade.java
+++ src/test/java/JavaClassPrograms/Interface/LetterGrade.java
@@ -3,11 +3,31 @@ package JavaClassPrograms.Interface;
 public class LetterGrade implements Grade{
     String lgrade;
 
-    switch (lgrade){
-        case lgrade.equalsIgnoreCase("A+"):
-            System.out.println(ha);
-    }
-    
+        String calculateletterGrade (String lgrade) {
+            switch (lgrade) {
+                case lgrade.equalsIgnoreCase("A+"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("A"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("B+"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("B"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("C+"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("C"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("D+"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("D"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("F+"):
+                    System.out.println("ha");
+                case lgrade.equalsIgnoreCase("F"):
+                    System.out.println("ha");
+            }
+        }
+
 
     @Override
     public int toPercent() {
